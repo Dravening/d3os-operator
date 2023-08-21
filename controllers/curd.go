eab0cde4589496c69c470f9b1452a79d3f70cd1f
draven <282864784@qq.com>
2023-08-21 15:24:18 +0800

    fix: createIfNotExist logic

--- controllers/curd.go
+++ controllers/curd.go
@@ -9,6 +9,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
@@ -21,29 +22,35 @@ func cmdCall(command string) error {
 	return nil
 }
 
+func createIfNotExist(r *DataServiceReconciler, ctx context.Context, objKey types.NamespacedName, objType, obj client.Object) error {
+	rLog := log.FromContext(ctx)
+	err := r.Get(ctx, objKey, objType)
+	if err != nil {
+		if !errors.IsNotFound(err) {
+			rLog.Error(err, fmt.Sprintf("getting obj %s error", objKey.String()))
+			return err
+		}
+		rLog.Info(fmt.Sprintf("obj %s not found, creating...", objKey.String()))
+		// 创建service
+		if err = r.Create(ctx, obj); err != nil {
+			rLog.Error(err, fmt.Sprintf("creating obj %s error", objKey.String()))
+			return err
+		}
+	}
+	// obj exists, do nothing
+	return nil
+}
+
 func createDeploymentIfNotExists(ctx context.Context, r *DataServiceReconciler, deploy *appsv1.Deployment) error {
 	if deploy == nil {
 		return fmt.Errorf("spec Deployment info doesn't exist, please check crd config")
 	}
-	rLog := log.FromContext(ctx)
 	deployTemp := &appsv1.Deployment{}
 	objKey := types.NamespacedName{
 		Name:      deploy.Name,
 		Namespace: deploy.Namespace,
 	}
-	err := r.Get(ctx, objKey, deployTemp)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			rLog.Info(fmt.Sprintf("deployment %s not found", objKey.String()))
-			return nil
-		}
-		rLog.Error(err, fmt.Sprintf("getting deployment %s error", objKey.String()))
-		return err
-	}
-	// 创建deployment
-	rLog.Info(fmt.Sprintf("creating deployment %s error", objKey.String()))
-	if err = r.Create(ctx, deploy); err != nil {
-		rLog.Error(err, fmt.Sprintf("creating deployment %s", objKey.String()))
+	if err := createIfNotExist(r, ctx, objKey, deployTemp, deploy); err != nil {
 		return err
 	}
 	return nil
@@ -53,25 +60,12 @@ func createStatefulSetIfNotExists(ctx context.Context, r *DataServiceReconciler,
 	if statefulSet == nil {
 		return fmt.Errorf("spec StatefulSet info doesn't exist, please check crd config")
 	}
-	rLog := log.FromContext(ctx)
 	stateTemp := &appsv1.StatefulSet{}
 	objKey := types.NamespacedName{
 		Name:      statefulSet.Name,
 		Namespace: statefulSet.Namespace,
 	}
-	err := r.Get(ctx, objKey, stateTemp)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			rLog.Info(fmt.Sprintf("statefulSet %s not found", objKey.String()))
-			return nil
-		}
-		rLog.Error(err, fmt.Sprintf("getting statefulSet %s error", objKey.String()))
-		return err
-	}
-	// 创建statefulSet
-	rLog.Info(fmt.Sprintf("creating statefulSet %s error", objKey.String()))
-	if err = r.Create(ctx, statefulSet); err != nil {
-		rLog.Error(err, fmt.Sprintf("creating statefulSet %s", objKey.String()))
+	if err := createIfNotExist(r, ctx, objKey, stateTemp, statefulSet); err != nil {
 		return err
 	}
 	return nil
@@ -81,25 +75,12 @@ func createDaemonSetIfNotExists(ctx context.Context, r *DataServiceReconciler, d
 	if daemonSet == nil {
 		return fmt.Errorf("spec DaemonSet info doesn't exist, please check crd config")
 	}
-	rLog := log.FromContext(ctx)
 	daemonTemp := &appsv1.DaemonSet{}
 	objKey := types.NamespacedName{
 		Name:      daemonSet.Name,
 		Namespace: daemonSet.Namespace,
 	}
-	err := r.Get(ctx, objKey, daemonTemp)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			rLog.Info(fmt.Sprintf("daemonSet %s not found", objKey.String()))
-			return nil
-		}
-		rLog.Error(err, fmt.Sprintf("getting daemonSet %s error", objKey.String()))
-		return err
-	}
-	// 创建daemonSet
-	rLog.Info(fmt.Sprintf("creating daemonSet %s error", objKey.String()))
-	if err = r.Create(ctx, daemonSet); err != nil {
-		rLog.Error(err, fmt.Sprintf("creating daemonSet %s", objKey.String()))
+	if err := createIfNotExist(r, ctx, objKey, daemonTemp, daemonSet); err != nil {
 		return err
 	}
 	return nil
@@ -109,25 +90,12 @@ func createServiceIfNotExists(ctx context.Context, r *DataServiceReconciler, ser
 	if service == nil {
 		return fmt.Errorf("spec Service info doesn't exist, please check crd config")
 	}
-	rLog := log.FromContext(ctx)
 	serviceTemp := &corev1.Service{}
 	objKey := types.NamespacedName{
 		Name:      service.Name,
 		Namespace: service.Namespace,
 	}
-	err := r.Get(ctx, objKey, serviceTemp)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			rLog.Info(fmt.Sprintf("service %s not found", objKey.String()))
-			return nil
-		}
-		rLog.Error(err, fmt.Sprintf("getting service %s error", objKey.String()))
-		return err
-	}
-	// 创建service
-	rLog.Info(fmt.Sprintf("creating service %s error", objKey.String()))
-	if err = r.Create(ctx, service); err != nil {
-		rLog.Error(err, fmt.Sprintf("creating service %s", objKey.String()))
+	if err := createIfNotExist(r, ctx, objKey, serviceTemp, service); err != nil {
 		return err
 	}
 	return nil
@@ -137,25 +105,12 @@ func createConfigMapIfNotExists(ctx context.Context, r *DataServiceReconciler, c
 	if configMap == nil {
 		return fmt.Errorf("spec ConfigMap info doesn't exist, please check crd config")
 	}
-	rLog := log.FromContext(ctx)
 	configMapTemp := &corev1.ConfigMap{}
 	objKey := types.NamespacedName{
 		Name:      configMap.Name,
 		Namespace: configMap.Namespace,
 	}
-	err := r.Get(ctx, objKey, configMapTemp)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			rLog.Info(fmt.Sprintf("configMap %s not found", objKey.String()))
-			return nil
-		}
-		rLog.Error(err, fmt.Sprintf("getting configMap %s error", objKey.String()))
-		return err
-	}
-	// 创建configMap
-	rLog.Info(fmt.Sprintf("creating configMap %s error", objKey.String()))
-	if err = r.Create(ctx, configMap); err != nil {
-		rLog.Error(err, fmt.Sprintf("creating configMap %s", objKey.String()))
+	if err := createIfNotExist(r, ctx, objKey, configMapTemp, configMap); err != nil {
 		return err
 	}
 	return nil
